0b51a631ed510faa792c2b6faa25252d26571fb0
danikCosiachniy <danilashumkevich@gmail.com>
2025-11-07 13:30:01 +0300

    change the structure to notebook file

--- spark_app/session.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession
-
-def get_spark(session_name: str) -> SparkSession: 
-    return (SparkSession.builder.appName(session_name)\
-            .config("spark.jars.packages", "org.postgresql:postgresql:42.7.4")\
-            .config("spark.sql.shuffle.partitions", "8")\
-            .getOrCreate() 
-            )
